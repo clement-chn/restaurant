7697e31fdb17f692651c42251309b3bf31ace30b
Clément Chouan <chouan.clement@gmail.com>
2023-05-09 14:36:09 +0200

    lutilisateur ne peut plus sélectionner plusieurs horaires pour une réservation

--- resources/js/changing-name-btn.js
+++ resources/js/changing-name-btn.js
@@ -1,24 +1,28 @@
 const scheduleButtons = document.querySelectorAll('button[name="time"]');
 const scheduleInput = document.getElementById('time-clicked-input');
-
-// let time = `${event.target.textContent}`;
-// console.log(scheduleButtons);
+let selectedButton = null;
 
 scheduleButtons.forEach(scheduleButton => {
-    scheduleButton.addEventListener('click', function(event) {
-      let button = event.target;
-      if (button.name === "time-clicked") {
-        console.log('le bouton est dé-sélectionné');
-        button.className = 'bg-gold w-16 p-3 text-white m-2 rounded';
-        button.name = "time";
-        scheduleInput.value = "";
-      } else {
-        console.log('le bouton est sélectionné');
-        button.className = 'bg-chocolate w-16 p-3 text-white m-2 rounded';
-        button.name = "time-clicked";
-        scheduleInput.value = button.value;
-      }
-    });
+  scheduleButton.addEventListener('click', function(event) {
+    let button = event.target;
+    if (selectedButton && selectedButton !== button) {
+      selectedButton.className = 'bg-gold w-16 p-3 text-white m-2 rounded';
+      selectedButton.name = "time";
+    }
+    if (button.name === "time-clicked") {
+      console.log('le bouton est dé-sélectionné');
+      button.className = 'bg-gold w-16 p-3 text-white m-2 rounded';
+      button.name = "time";
+      scheduleInput.value = "";
+      selectedButton = null;
+    } else {
+      console.log('le bouton est sélectionné');
+      button.className = 'bg-chocolate w-16 p-3 text-white m-2 rounded';
+      button.name = "time-clicked";
+      scheduleInput.value = button.value;
+      selectedButton = button;
+    }
   });
+});
 
   
